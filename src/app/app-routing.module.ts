1322fed0eeccfe0b3c190f2cc9541150b9b64325
André de Jesus <andre.dejesus.9@gmail.com>
2020-01-26 17:14:25 -0300

    primeiro commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
+import { NovoServicoComponent } from './servicos/novo-servico/novo-servico.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'servicos/novo', component: NovoServicoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarraNavegacaoComponent } from './barra-navegacao/barra-navegacao.component';
+import { NovoServicoComponent } from './servicos/novo-servico/novo-servico.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraNavegacaoComponent,
+    NovoServicoComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
